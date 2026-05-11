5e641aac82501b5b01c09cf73be69405c20c4f68
AlanGomez-Programmer <alan.s.g.j.2006@gmail.com>
2026-05-10 23:23:45 -0600

    feat: :zap: Se mejoro la forma de registrar id y nombre de tipo de tarea
    Ahora se pondra el id manualmente para que el usuario ingrese el numero de id, antes de registrar el id, se mostrar un listado de ids y nombres ya resgistados

--- gestion_tipo_tarea.py
+++ gestion_tipo_tarea.py
@@ -1,23 +1,64 @@
 import funcionalidades
 
+def listar_tipos():
+    tipos_tareas = funcionalidades.leer_archivo(funcionalidades.archivos["tipo_tarea"])
+
+    for id, tipo in tipos_tareas.items():
+        print(f"  {id}. {tipo}")
+
+def validar_id():
+    while True:
+        tipos = funcionalidades.leer_archivo(funcionalidades.archivos["tipo_tarea"])
+        print("="*50)
+        print("          REGISTRO DE TIPO DE TAREA")
+
+        if tipos == {}:
+            print("Aún no hay tipos de tareas registradas")
+            print("Realiza tu primer registro de tipo de tarea")
+        else:
+            print("  Tipos de tareas ya registrados:")
+            listar_tipos()
+
+        while True:
+            id_existentes = [int(id_e) for id_e in tipos]
+
+            try:
+                id = int(input("\n  ID: ").strip())
+
+                if id < 0:
+                    print("  Error: Ingrese otro nuemero de Id mayor a 0 ")
+                    continue
+                elif id > len(tipos)+1:
+                    print("  Error: El Id debe tener la misma secuencia")
+                    continue
+                elif id in id_existentes:
+                    print("  Eror: ese id ya existe")
+                    continue
+                else:
+                    return id
+            except ValueError:
+                print("  Error: Solo se perminten números")
+                continue
+
 def tipo_tarea():
     funcionalidades.limpiar_pantalla()
-    print("="*50)
-    print("          REGISTRO DE TIPO DE TAREA")
-    while True:    
+    tipos = funcionalidades.leer_archivo(funcionalidades.archivos["tipo_tarea"])
+
+    id = validar_id()
+
+    while True:
         Nombre_tipo = input(" \n  Nombre de tipo de tarea: ").strip().capitalize()
-
-        tipos = funcionalidades.leer_archivo(funcionalidades.archivos["tipo_tarea"])
-
+        
         existe = any(Nombre_tipo == tipo for tipo in tipos.values())
 
         if existe:
             print("  Error: Ese nombre ya existe")
             continue
         else:
-            funcionalidades.crear(funcionalidades.archivos["tipo_tarea"], Nombre_tipo)
+            tipos[id] = Nombre_tipo
+            funcionalidades._guardar_datos(funcionalidades.archivos["tipo_tarea"], tipos)
             print("  Tipo de tarea agregada exitosamente")
             print("="*50)    
             input("Presione ENTER para salir...")        
             return
-            
+           
