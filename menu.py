bb9aeeba5bc711dc2231c5c2db310ad19dd96052
AlanGomez-Programmer <alan.s.g.j.2006@gmail.com>
2026-05-11 14:11:24 -0600

    refactor: :zap: Se movio el menu al ciclo para que muestre menu al devolverse

--- menu.py
+++ menu.py
@@ -3,16 +3,16 @@ from gestion_tareas import agregar_tarea
 from gestion_tipo_tarea import tipo_tarea
 
 def menu_pricipal():
-    funcionalidades.limpiar_pantalla()
-    print("="*50)
-    print("                      MENU\n")
-    print("  1. Agregar Nueva Tarea")
-    print("  2. Ver tareas")
-    print("  3. Cambiar Estado de Tarea")
-    print("  4. Regristrar Tipo de Tarea")
-    print("  5. Ver Tipo de Tarea Registada\n")
-    print("="*50)
     while True: 
+        funcionalidades.limpiar_pantalla()
+        print("="*50)
+        print("                      MENU\n")
+        print("  1. Agregar Nueva Tarea")
+        print("  2. Ver tareas")
+        print("  3. Cambiar Estado de Tarea")
+        print("  4. Regristrar Tipo de Tarea")
+        print("  5. Ver Tipo de Tarea Registada\n")
+        print("="*50)
         try:
             opci = int(input("Ingrese opción elegida: ").strip())
 
