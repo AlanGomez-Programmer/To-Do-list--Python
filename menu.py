8f5c5d3091fc21590b1d2116e55d5e9a3038af1b
AlanGomez-Programmer <alan.s.g.j.2006@gmail.com>
2026-05-11 14:01:28 -0600

    feat: :sparkles: Se agrega la función menu
    Se agrega la función del menu pricipal, solo se agregaron dos funciones para que funcione el menu

---
+++ menu.py
@@ -0,0 +1,37 @@
+import funcionalidades
+from gestion_tareas import agregar_tarea
+from gestion_tipo_tarea import tipo_tarea
+
+def menu_pricipal():
+    funcionalidades.limpiar_pantalla()
+    print("="*50)
+    print("                      MENU\n")
+    print("  1. Agregar Nueva Tarea")
+    print("  2. Ver tareas")
+    print("  3. Cambiar Estado de Tarea")
+    print("  4. Regristrar Tipo de Tarea")
+    print("  5. Ver Tipo de Tarea Registada\n")
+    print("="*50)
+    while True: 
+        try:
+            opci = int(input("Ingrese opción elegida: ").strip())
+
+            if opci == 1:
+                funcionalidades.limpiar_pantalla()
+                agregar_tarea()
+            elif opci == 2:
+                funcionalidades.limpiar_pantalla()
+            elif opci == 3: 
+                funcionalidades.limpiar_pantalla()
+            elif opci == 4:
+                funcionalidades.limpiar_pantalla()
+                tipo_tarea()
+            elif opci == 5:
+                funcionalidades.limpiar_pantalla()
+            else:
+                print("Error: Esa opción no existe")
+                continue
+        except ValueError:
+            print("Error: Solo se permiten números")
+
+menu_pricipal()
